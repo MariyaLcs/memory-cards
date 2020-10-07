2e69f0252cc20eec5c90f77cc90ec503f2efe467
MariyaLcs <mariya.podosinova@gmail.com>
2020-10-07 15:07:05 +0100

    start point

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordion from "./components/Accordion";
 
 function App() {
+  const items = [
+    { title: "Family", content: "Where are you from originally?" },
+    { title: "Occupation", content: "What do they do for a living?" },
+    { title: "Recreation", content: "What do they do for fun?" },
+    { title: "Dreams", content: "What are your plans for the next year?" },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion items={items} />
     </div>
   );
 }
