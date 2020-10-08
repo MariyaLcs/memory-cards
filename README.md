026eacf609357c6f46641a2a63bb2b6df5249a1e
MariyaLcs <mariya.podosinova@gmail.com>
2020-10-08 13:58:21 +0100

    executing the Request from useEffect, adding conditional for default null search

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ### Main Parts of the course project
 
-1. Wikipedia API
-   https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch='New_England_Patriots'
+1. useState()
+2. useEffect()
+3. Wikipedia API
+   https://en.wikipedia.org/w/api.php
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  console.log(results);
+  useEffect(() => {
+    //1 way - create a helper fn
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+    if (term) {
+      search();
+    }
+    //2 way
+    // (async () => {
+    //   await axios.get("");
+    // })();
+    //3 way
+    // axios.get("adf").then((res) => {
+    //   console.log(res);
+    // });
+  }, [term]);
   return (
     <div>
       <div className="ui form">
