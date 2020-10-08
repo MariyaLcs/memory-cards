d3e017bfea7e42aad5ab8cad1e9eeb392e124c72
MariyaLcs <mariya.podosinova@gmail.com>
2020-10-08 09:40:14 +0100

    adding onClick function

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => {
+  const renderedItems = items.map((item, index) => {
     return (
       <React.Fragment key={item.title}>
-        <div className="title name">
-          <i className="dropdown icon">{item.title}</i>
+        <div
+          className="title active"
+          onClick={() => console.log("title clicked", index)}
+        >
+          <i className="dropdown icon"></i>
+          {item.title}
         </div>
         <div className="content active">
           <p>{item.content}</p>
