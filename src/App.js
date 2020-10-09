6ff219846a1d627cfb7fd3a7528c838ebee7b10d
MariyaLcs <mariya.podosinova@gmail.com>
2020-10-09 13:13:28 +0100

    adding hiding and showing the Optional List functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 
-class App extends React.Component {
-  render() {
-    const items = [
-      { title: "Family", content: "Where are you from originally?" },
-      { title: "Occupation", content: "What do they do for a living?" },
-      { title: "Recreation", content: "What do they do for fun?" },
-      { title: "Dreams", content: "What are your plans for the next year?" },
-    ];
-    const options = [
-      { label: "London", value: "option1" },
-      { label: "Moscow", value: "option2" },
-      { label: "Tokio", value: "option13" },
-    ];
-    return (
-      <div>
-        <Dropdown options={options} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  const items = [
+    { title: "Family", content: "Where are you from originally?" },
+    { title: "Occupation", content: "What do they do for a living?" },
+    { title: "Recreation", content: "What do they do for fun?" },
+    { title: "Dreams", content: "What are your plans for the next year?" },
+  ];
+  const options = [
+    { label: "London", value: "option1" },
+    { label: "Moscow", value: "option2" },
+    { label: "Tokio", value: "option3" },
+  ];
+  const [selected, setSelected] = useState(options[0]);
+  return (
+    <div>
+      <Dropdown
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+      />
+    </div>
+  );
+};
 
 export default App;
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Dropdown = () => {
-  return <div className="">Dropdown</div>;
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+  const renderedOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null;
+    }
+    return (
+      <div
+        key={option.value}
+        className="item"
+        onClick={() => onSelectedChange(option)}
+      >
+        {option.value}
+      </div>
+    );
+  });
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label className="label">Select a City</label>
+        <div
+          className={`ui selection dropdown ${open && "visible active"}`}
+          onClick={() => setOpen(!open)}
+        >
+          <i className="dropdown icon"></i>
+          <div className="text">{selected.label}</div>
+          <div className={`menu ${open && "visible transition"}`}>
+            {renderedOptions}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dropdown;
