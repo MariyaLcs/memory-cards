84d8ae5cf42cf87ab610924279a0efbaf8f3f568
MariyaLcs <mariya.podosinova@gmail.com>
2020-10-10 21:53:12 +0100

    building a reusable route component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
 
 const App = () => {
   const items = [
@@ -16,33 +17,27 @@ const App = () => {
     { label: "Moscow", value: "Moscow" },
     { label: "Tokio", value: "Tokio" },
   ];
-  const showAccordion = () => {
-    if (window.location.pathname === "/") {
-      return <Accordion items={items} />;
-    }
-  };
-  const showList = () => {
-    if (window.location.pathname === "/list") {
-      return <Search />;
-    }
-  };
-  const showDropdown = () => {
-    if (window.location.pathname === "/dropdown") {
-      return <Dropdown />;
-    }
-  };
-  const showTranslate = () => {
-    if (window.location.pathname === "/translate") {
-      return <Translate />;
-    }
-  };
 
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      {showAccordion()}
-      {showList()}
-      {showDropdown()}
-      {showTranslate()}
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a City"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
